a8bc4417e00e06d0f10ca1d633bdea8d5c520bcc
oassise <oassishelp@gmail.com>
2025-07-17 19:34:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const cors = require('cors');
+//const cors = require('cors');
 
 const app = express();
 //app.use(cors({ origin: 'https://oassisjob.web.app' }));
